0cc46cc5926ec4ab9bab39ac167dbe0ed8452ff0
champ18ion <113764445+champ18ion@users.noreply.github.com>
2022-11-28 12:03:02 +0530

    1 file modified, 3 added

--- all-files/3-modules.js
+++ all-files/3-modules.js
@@ -3,6 +3,9 @@
 // Modules - Encapsulated Code (only share minimum)
 const names = require('./4-names')
 const sayHi = require('./5-utility')
+const data = require('./6-alternate-flav')
+require('./7-mind-greanade');
+console.log(data);
 console.log(names);
 sayHi('susan')
 sayHi(names.john)
