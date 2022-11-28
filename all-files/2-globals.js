e399ce5db235643b566c58e691ab0831dfa3c759
champ18ion <113764445+champ18ion@users.noreply.github.com>
2022-11-28 10:59:57 +0530

    3 files added

---
+++ all-files/2-globals.js
@@ -0,0 +1,14 @@
+
+// GLOBALS - NO WINDOW !!!!
+// _dirname - path to current directory
+//__filename- file name.
+// require - function to use modules (CommonJS)
+// module - info about current module (file)
+//process - info about env where the program is being executed
+
+// console.log(__dirname);
+
+// print using setInteral
+setInterval(()=>{
+  console.log('hello node.!')
+},2000);pw
