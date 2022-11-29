433f618ee8046e481f52fd32017ccce887fe5f24
champ18ion <113764445+champ18ion@users.noreply.github.com>
2022-11-29 12:28:06 +0530

    1 file modified, 9 added

--- all-files/app.js
+++ all-files/app.js
@@ -1,17 +1,14 @@
-const os = require('os');
+const http = require('http');
 
-// info about current user
-const user = os.userInfo()
-console.log(user)
-
-// return system uptime in sec
-console.log(`the system uptime is ${os.uptime()}`)
-
-// os info
-const currentOS = {
-  name: os.type(),
-  release: os.release(),
-  totalMem: os.totalmem(),
-  freeMem: os.freemem()
-  } 
-  console.log(currentOS)
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.end('Welcome to our home page');
+  }
+  if (req.url === '/about') {
+    res.end('Here is our short history');
+  }
+  res.end(`<h1>Oops!</h1>
+  <p>We can't seem to find the page you are looking for</p>
+  <a href="/">back home</a>`);
+});
+server.listen(5000);
