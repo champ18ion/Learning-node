6d2910cb198b05d5565fd8c39b18b7d2dfaf4148
Kunal Kumar <113764445+champ18ion@users.noreply.github.com>
2022-12-10 11:35:55 +0530

    static using express

---
+++ express/all-files/05-all-static.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const path = require('path');
+const app = express();
+
+// put all static things at once
+app.use(express.static('./navbar-app'))
+
+// app.get('/',(req,res)=>{
+//   res.sendFile(path.resolve(__dirname,'./all-files/navbar-app/index.html'))
+// })
+app.get('/about',(req,res)=>{
+  res.send('about page')
+})
+app.all('*',(req,res)=>{
+  res.status(404).send('<h1>resource not found</h1>')
+})
+
+app.listen(5000,()=>{
+  console.log('server listening on port: 5000')
+})
--- express/app.js
+++ express/app.js
@@ -1,16 +1,35 @@
 const express = require('express')
-const app = express();
+const app = express()
+const { products } = require('./data')
 
-app.get('/',(req,res)=>{
-  res.status(200).send('Home page')
-})
-app.get('/about',(req,res)=>{
-  res.send('about page')
-})
-app.all('*',(req,res)=>{
-  res.status(404).send('<h1>resource not found</h1>')
+ 
+app.get('/', (req, res) => {
+  res.send('<h1>Home Page</h1><a href="api/products"> products </a>')
 })
 
-app.listen(5000,()=>{
-  console.log('server listening on port: 5000')
+app.get('/api/products',(req,res) => {
+  const newProduct = products.map((product)=>{
+    const{id,name,image}=product
+    return{id,name,image}
+  })
+  res.json(newProduct)
+
+})
+// app.get('/api/products/1',(req,res) => {
+//   const singleProduct = products.find((product)=>product.id===1)
+//   res.json(singleProduct)
+//   })
+
+// routing method
+app.get('/api/products/:productID',(req,res) => {
+  console.log(req.params);
+  const {productID} = req.params;
+  const singleProduct = products.find((product)=>product.id===Number(productID))
+   res.json(singleProduct)
+  }) 
+
+
+
+app.listen(5000, () => {
+  console.log('Server is listening on port 5000....')
 })
