f034e5dc5e8b32aa3e8d83984343104132b89363
Kunal Kumar <113764445+champ18ion@users.noreply.github.com>
2022-12-12 11:14:32 +0530

    post method

--- express/app.js
+++ express/app.js
@@ -1,27 +1,11 @@
 const express = require('express')
 const app = express()
-const morgan = require('morgan')
-const logger = require('./all-files/logger/logger')
-const authorise = require('./all-files/logger/authorise')
-//  req => middleware => res
+let { people } = require('./data')
 
-// app.use([logger, authorize])
+// static assets
+app.use(express.static('./all-files/methods-public'))
 // app.use(express.static('./public'))
-app.use(morgan('tiny'))
 
-app.get('/', (req, res) => {
-  res.send('Home')
-})
-app.get('/about', (req, res) => {
-  res.send('About')
-})
-app.get('/api/products', (req, res) => {
-  res.send('Products')
-})
-app.get('/api/items', (req, res) => {
-  console.log(req.user)
-  res.send('Items')
-})
 
 app.listen(5000, () => {
   console.log('Server is listening on port 5000....')
